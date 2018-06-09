d9cd8b8404b1d4abb038a2c0dd860df19791a611
autotestsozon <autotestsozon@gmail.com>
2018-06-09 14:08:00 +0300

    Задание №15: реализовать загрузку тестовых данных из файла

--- addressbook-web-tests/addressbook-web-tests/Tests/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/ContactCreationTests.cs
@@ -2,42 +2,54 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 using System.Threading;
 using NUnit.Framework;
+using System.IO;
 
 namespace WebAddressbookTests 
 {
     [TestFixture]
     public class ContactCreationTests : AuthTestBase
     {
-        //public static IEnumerable<ContactData> RandomContactDataProvider()
-        //{
-        //    List<ContactData> contacts = new List<ContactData>();
-        //    for (int i = 0; i < 5; i++)
-        //    {
-        //        contacts.Add(new ContactData(GenerateRandomString(10))
-        //        {
-        //            MiddleName = GenerateRandomString(10),
-        //            Lastname = GenerateRandomString(10)
-        //        });
-        //    }
-        //    return contacts;
-        //}
-
-        //[Test, TestCaseSource("RandomContactDataProvider")]
-        [Test]
-        //public void ContactCreationTest(ContactData contact)
-        public void ContactCreationTest()
+        public static IEnumerable<ContactData> RandomContactDataProvider()
         {
-            ContactData newcontact = new ContactData("Name");
-            newcontact.Lastname = ("Fam");
+            List<ContactData> contacts = new List<ContactData>();
+            for (int i = 0; i < 5; i++)
+            {
+                contacts.Add(new ContactData(GenerateRandomString(10))
+                {
+                    MiddleName = GenerateRandomString(10),
+                    Lastname = GenerateRandomString(10)
+                });
+            }
+            return contacts;
+        }
 
+        public static IEnumerable<ContactData> ContactDataFromXmlFile()
+        {
+            List<ContactData> groups = new List<ContactData>();
+            return (List<ContactData>)new XmlSerializer(typeof(List<ContactData>))
+                .Deserialize(new StreamReader(@"contacts.xml"));
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromJsonFile()
+        {
+            return JsonConvert.DeserializeObject<List<ContactData>>(
+                File.ReadAllText(@"contacts.json"));
+        }
+
+        [Test, TestCaseSource("ContactDataFromJsonFile")]
+        public void ContactCreationTest(ContactData contact)
+        {
             List<ContactData> oldContacts = app.Contact.GetContactList();
 
-            app.Contact.Create(newcontact);
+            app.Contact.Create(contact);
 
             List<ContactData> newContacts = app.Contact.GetContactList();
-            oldContacts.Add(newcontact);
+            oldContacts.Add(contact);
             newContacts.Sort();
             oldContacts.Sort();
             Assert.AreEqual(oldContacts, newContacts);
